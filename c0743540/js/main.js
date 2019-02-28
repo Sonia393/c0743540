fad3d8d4a2a32c0ae02f2ab2c733304348ad657c
visualstudio <visualstudio@230-PC23>
2019-02-28 11:22:54 -0500

    done

--- c0743540/js/main.js
+++ c0743540/js/main.js
@@ -1,1 +1,24 @@
 ﻿// Your code here!
+function createcircle(radius) {
+    return {
+        radius,
+        draw: function() {
+            console.log('draw');
+        }
+    };
+}
+const circle = createcircle(1);
+circle.draw();
+
+/*const circle = {
+    radius: 1,
+    location: {
+        x: 1, y: 1
+    },
+    draw: function () {
+        console.log('draw');
+    }
+
+
+};
+circle.draw();*/
